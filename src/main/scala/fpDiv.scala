2678e535e5353dc94cc08f37627877f6368b1df9
platelets <wenkanedward@hust.edu.cn>
2024-08-20 01:34:44 +0800

    change to chisel version 6 but has problem

---
+++ src/main/scala/fpDiv.scala
@@ -0,0 +1,49 @@
+package fpDivision
+
+import chisel3._
+import chisel3.util._
+// import chisel3.iotesters.{PeekPokeTester, Driver, ChiselFlatSpec}
+
+class fpDiv(val w: Int) extends Module { //w = 32
+  val io = IO(new Bundle {
+    val in1 = Input(UInt(width = w.W))
+    val in2 = Input(UInt(width = w.W))
+    val out = Output(UInt(width = w.W))
+  })
+
+  val inverter = Module(new fpInverter(23))
+  val multiplier = Module(new FPMult(w))
+
+  inverter.io.in1 := io.in2(22, 0) //mantissa
+  //val exponent2    = io.in2(30, 23) - 127.U
+  //val negExpTmp    = 127.U - exponent2		// invert the sign of exponent
+  val negExpTmp = 254.U - io.in2(30, 23)
+  val invMant = inverter.io.out(23, 0)
+  val negExp = Mux(invMant === 0.U, negExpTmp, negExpTmp - 1.U)
+
+  // we should raise an execption if both mantissa and exponent are zero (the final result should be inf
+
+  //val in1Buffer    = Reg(init = 0.U, next = io.in1)
+
+  multiplier.io.a := io.in1
+  multiplier.io.b := Cat(io.in2(31), negExp, inverter.io.out(23, 1))
+  // skipping msb of inverter (multiplying mantissa by 2)
+
+  io.out := multiplier.io.res
+
+  printf("\nmultiplier b : %d\n", Cat(io.in2(31), negExp, inverter.io.out(23, 1)))
+  //printf("exponent2: %d\n", exponent2)
+  printf("negExp : %d, in1 exp %d\n", negExp, io.in1(30, 23))
+  printf("in1: %d, in2: %d\n", io.in1, io.in2)
+  printf("inverter in: %d out: %d\n", io.in2(22, 0), inverter.io.out)
+  printf("out: %d\n", multiplier.io.res)
+
+//	printf("%d\n", multiplier.io.res)
+
+}
+
+// object fpDiv {
+//   def main(args: Array[String]): Unit = {
+//     if (!Driver(() => new fpDiv(32))(c => new fpDivTest(c))) System.exit(1)
+//   }
+// }
